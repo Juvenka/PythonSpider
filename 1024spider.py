4daa72afceac387286a9e4e2b0952afbc2cfce69
Juvenka <Juvenka@users.noreply.github.com>
2017-10-25 15:50:40 +0800

    Initial commit

---
+++ 1024spider.py
@@ -0,0 +1,29 @@
+import urllib.request
+import bs4
+import collections
+
+firsturl = ["http://1024.2048xd.info/pw/htm_data/3/1710/835746.html"]
+will_see = collections.deque(firsturl)
+saw = set()
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36'}
+
+while True:
+    web = will_see.popleft()
+    if web not in saw:
+        urlop = urllib.request.Request(web,headers = headers)
+    else:
+        continue
+
+    h = urllib.request.urlopen(urlop)
+    saw.add(web)
+    html_web = bs4.BeautifulSoup(h,'lxml')
+    target = html_web.find(name='div',attrs={'class':'tpc_content','id':'read_tpc'}).find_all(name='img',attrs={'border':'0'})
+    i = 0
+    for p in target:
+        sex_pic = urllib.request.urlopen(p['src'])
+        i += 1
+        f = open(str(i)+'.jpeg','wb')
+        f.write(sex_pic.read())
+        f.close()
+    if len(will_see) == 0:
+        break
