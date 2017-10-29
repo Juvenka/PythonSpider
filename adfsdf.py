05fa90bac67939abf5c2b3f9b1f88a2f9c5e52c4
Juvenka <32841653+Juvenka@users.noreply.github.com>
2017-10-29 23:05:44 +0800

    l

--- adfsdf.py
+++
@@ -1,7 +0,0 @@
-import io
-import sys
-import urllib.request
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf8') #改变标准输出的默认编码
-res=urllib.request.urlopen('http://www.baidu.com')
-htmlBytes=res.read()
-print(htmlBytes.decode('utf-8'))
