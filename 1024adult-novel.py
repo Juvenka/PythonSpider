b747805d513a1c9ccf4c583dc03c2ba61414c0ec
Juvenka <32841653+Juvenka@users.noreply.github.com>
2017-10-30 23:00:25 +0800

    4

--- 1024adult-novel.py
+++ 1024adult-novel.py
@@ -1,12 +1,10 @@
 import urllib.request
 import bs4
-import collections
 import os
-
+import socket
 firsturl = "http://1024.2048xd.info/pw/thread.php?fid=17"
-will_see = collections.deque()
-saw = set()
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/61.0.3163.79 Safari/537.36'}
 
 URL = urllib.request.Request(firsturl, headers=headers)
 h = urllib.request.urlopen(URL)
@@ -22,29 +20,34 @@ for t in target:
     num[str(i)] = t.string
     i += 1
 print('请输入序号：', end=' ')
-
-halfTybe = num[input()].parent['href']
+series = num[input()]
+halfTybe = series.parent['href']
 Tybe = Referer + halfTybe
 Tybe_url = urllib.request.Request(Tybe, headers=headers)
 Tybe_open = urllib.request.urlopen(Tybe_url)
 Tybe_page = bs4.BeautifulSoup(Tybe_open, 'html5lib', from_encoding='UTF-8')
 tar_tag = Tybe_page.find_all(name='a', attrs={'href': True, 'id': True})
 i = 0
-if not os.path.exists('C:\\Users\\Administrator\\Desktop\\erotic_pics'):
-    os.mkdir('C:\\Users\\Administrator\\Desktop\\erotic_pics')
+if not os.path.exists('C:\\Users\\Administrator\\Desktop\\erotic_novels'):
+    os.mkdir('C:\\Users\\Administrator\\Desktop\\erotic_novels')
 for tar in tar_tag:
     i += 1
     if i <= 6:
         continue
     else:
-        f = open('C:\\Users\\Administrator\\Desktop\\erotic_pics' + os.sep + tar.string + '.txt', 'w')
+        f = open('C:\\Users\\Administrator\\Desktop\\erotic_novels' + os.sep + series + tar.string + '.txt', 'w')
         erotic_novel = urllib.request.Request(Referer+tar['href'], headers=headers)
-        erotic_novel_open = urllib.request.urlopen(erotic_novel)
-        erotic_novel_html = bs4.BeautifulSoup(erotic_novel_open, 'lxml', from_encoding='UTF-8')
-
-        novel = erotic_novel_html.find(name='div', attrs={'class': 'tpc_content', 'id': 'read_tpc'})
+        try:
+            erotic_novel_open = urllib.request.urlopen(erotic_novel,timeout=10)
+            erotic_novel_html = bs4.BeautifulSoup(erotic_novel_open, 'lxml', from_encoding='UTF-8')
+            novel = erotic_novel_html.find(name='div', attrs={'class': 'tpc_content', 'id': 'read_tpc'})
+        except socket.timeout as e:
+            continue
         for nov in novel.stripped_strings:
-            f.write('    '+nov.replace(u'\xa0', u' ')+'\n')
+            nov = nov.replace('\xa0' , ' ')
+            nov = nov.replace('\ue0ff', ' ')
+            nov = nov.replace('\ue197', ' ')
+            f.write('    '+nov+'\n\n')
         f.close()
-        print('第'+str(i-6)+'篇下载完毕')
+        print('第'+str(i-6)+'篇'+tar.string+'下载完毕')
 print('第一页'+str(i-6)+'下载完')
