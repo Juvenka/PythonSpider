d4d74ddf1a0fe7f134c6c043558c6256210028a7
Juvenka <Juvenka@users.noreply.github.com>
2017-11-10 15:54:56 +0800

    11.10

--- zhihu_spider/zhihu_topic.py
+++ zhihu_spider/zhihu_topic.py
@@ -1,12 +1,13 @@
-import urllib.request
 from bs4 import BeautifulSoup
-import http
-url = 'https://www.zhihu.com/topic/19776749/organize/entire'
+import requests.auth
+url = 'https://www.zhihu.com/signin?next=%2F%3Fnext%3D%252Ftopic%252F19776749%252Forganize%252Fentire'
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'}
+zhihu_html = requests.get(url,headers=headers,auth=requests.auth.HTTPBasicAuth('362696076@qq.com', 'Zxw1213#'))
 
-zhihu_html = urllib.request.urlopen(url)
-print(zhihu_html.headers['vary'])
-
-zhihu_soup = BeautifulSoup(zhihu_html,'lxml')
-topic = zhihu_soup.find_all(name='ul',attrs={'class':'zm-topic-organize-list'})
-print(zhihu_soup)
+zhihu_soup = BeautifulSoup(zhihu_html.content,'html5lib')
+topic = zhihu_soup.find(name='div',attrs={'id':'zh-topic-organize-page-children'})
 print(topic)
+myname = zhihu_soup.body.find('span',attrs={'class':'name'})
+print(myname.string)
+print(zhihu_html.url)
+print(zhihu_html.history)
