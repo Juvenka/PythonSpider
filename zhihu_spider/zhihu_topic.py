22d0b29bdfe0a062c4e3b2f4b0ccdf4c5c404fce
Juvenka <Juvenka@users.noreply.github.com>
2017-11-13 16:04:40 +0800

    11.13

--- zhihu_spider/zhihu_topic.py
+++ zhihu_spider/zhihu_topic.py
@@ -1,13 +1,14 @@
 from bs4 import BeautifulSoup
-import requests.auth
-url = 'https://www.zhihu.com/signin?next=%2F%3Fnext%3D%252Ftopic%252F19776749%252Forganize%252Fentire'
+import requests
+s = requests.session()
+url = 'https://www.zhihu.com/login/email'
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'}
-zhihu_html = requests.get(url,headers=headers,auth=requests.auth.HTTPBasicAuth('362696076@qq.com', 'Zxw1213#'))
-
-zhihu_soup = BeautifulSoup(zhihu_html.content,'html5lib')
-topic = zhihu_soup.find(name='div',attrs={'id':'zh-topic-organize-page-children'})
-print(topic)
-myname = zhihu_soup.body.find('span',attrs={'class':'name'})
-print(myname.string)
-print(zhihu_html.url)
-print(zhihu_html.history)
+cookies = {'account':'362696076@qq.com','password':'Zxw1213#','_xsrf':'6f7f24b92e8dcde6b5760b4436e13d43'}
+zhihu_html1 = s.post(url,headers=headers,data=cookies)
+print(zhihu_html1)
+zhihu_soup = BeautifulSoup(zhihu_html1.content,'html5lib')
+print(zhihu_soup)
+r = s.get(url,headers=headers)
+print(r)
+zhihu_soup = BeautifulSoup(r.content,'html5lib')
+print(zhihu_soup)
