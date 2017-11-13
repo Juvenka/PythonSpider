fe5fa2a907c99000decf27074ff70dda70880745
Juvenka <32841653+Juvenka@users.noreply.github.com>
2017-11-13 22:55:17 +0800

    1

--- zhihu_spider/zhihu_topic.py
+++ zhihu_spider/zhihu_topic.py
@@ -3,12 +3,17 @@ import requests
 s = requests.session()
 url = 'https://www.zhihu.com/login/email'
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'}
-cookies = {'account':'362696076@qq.com','password':'Zxw1213#','_xsrf':'6f7f24b92e8dcde6b5760b4436e13d43'}
-zhihu_html1 = s.post(url,headers=headers,data=cookies)
+cookies = {'account': '362696076@@qq.com', 'password': 'Zxw1213#', '_xsrf': '30646533636334372d306162382d343336652d396433312d633331363263356234656164'}
+zhihu_html1 = s.post(url, data=cookies, headers=headers)
 print(zhihu_html1)
 zhihu_soup = BeautifulSoup(zhihu_html1.content,'html5lib')
 print(zhihu_soup)
-r = s.get(url,headers=headers)
-print(r)
-zhihu_soup = BeautifulSoup(r.content,'html5lib')
-print(zhihu_soup)
+
+zhihu_html = s.get('https://www.zhihu.com/people/edit',headers=headers)
+soup = BeautifulSoup(zhihu_html.content,'html5lib')
+print(soup)
+myname = soup.find('span', attrs={"class": "Field-text",'data-reactid':'133'})
+print(myname)
+#<div id="Popover-43436-11071-toggle" aria-haspopup="true" aria-expanded="false" aria-owns="Popover-43436-11071-content">机器学习</div>
+#<div aria-expanded="false" aria-haspopup="true" aria-owns="null-content" data-reactid="150" id="null-toggle">数据分析</div>
+#<span class="Field-text" data-reactid="133">临床医疗</span>
--- zhihu_spider/zhihu_topic1.py
+++ zhihu_spider/zhihu_topic1.py
@@ -10,15 +10,16 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) '
                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36'
 }
-postdata = {'account': '362696076@qq.com', 'password': 'Zxw1213#'}
+postdata = {'account': '13585884094', 'password': 'Zxw1213#'}
 response = session.get("https://www.zhihu.com", headers=headers)
 soup = BeautifulSoup(response.content, "html.parser")
 xsrf = soup.find('input', attrs={"name": "_xsrf"}).get("value")
 postdata['_xsrf'] =xsrf
-result = session.post('http://www.zhihu.com/login/email', data=postdata, headers=headers)
+result = session.post('https://www.zhihu.com/login/phone_num', data=postdata, headers=headers)
 session.cookies.save(ignore_discard=True, ignore_expires=True)
 
-zhihu_html = session.get('https://www.zhihu.com/people/beiki/activities',headers=headers)
+zhihu_html = session.get('https://www.zhihu.com/people/edit',headers=headers)
 soup = BeautifulSoup(zhihu_html.content,'html5lib')
-myname = soup.find('span', attrs={"class": "ProfileHeader-name"})
-print(myname.string)
+print(soup)
+myname = soup.find('span', attrs={"class": "Field-text",'data-reactid':'133'})
+print(myname)
