3ce5654e4a08960141e401b69b8b259b7a43470f
Juvenka <Juvenka@users.noreply.github.com>
2017-11-14 13:41:11 +0800

    1

--- zhihu_spider/xiaojudeng.py
+++ zhihu_spider/xiaojudeng.py
@@ -7,11 +7,11 @@ headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) '
 password = {'username':'juvenbaby','password':'bnm1235'}
 zhihu_html_1 = s.post(url,headers=headers,data=password)
 
-zhihu_html = s.get('http://www.iivd.net',headers=headers)
+zhihu_html = s.get('http://www.iivd.net/home.php?mod=space&uid=6682&do=profile&from=space',headers=headers)
 zhihu_soup = BeautifulSoup(zhihu_html.content,'html5lib')
 print(zhihu_soup)
-topic = zhihu_soup.find(name='a',attrs={'href':True,'target':'_blank','title':'访问我的空间'})
-print(topic.string)
+topic = zhihu_soup.find(name='ul',attrs={'class':'pf_l cl'})
+print(topic)
 
 #fastloginfield:username
 #username:juvenbaby
--- zhihu_spider/zhihu_topic.py
+++ zhihu_spider/zhihu_topic.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+
 s = requests.session()
 url = 'https://www.zhihu.com/login/email'
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'}
@@ -9,11 +10,10 @@ print(zhihu_html1)
 zhihu_soup = BeautifulSoup(zhihu_html1.content,'html5lib')
 print(zhihu_soup)
 
-zhihu_html = s.get('https://www.zhihu.com/people/edit',headers=headers)
+zhihu_html = s.get('https://www.zhihu.com/topic/19776749/organize/entire',headers=headers)
 soup = BeautifulSoup(zhihu_html.content,'html5lib')
 print(soup)
-myname = soup.find('span', attrs={"class": "Field-text",'data-reactid':'133'})
+myname = soup.find('div', attrs={"class": "zm-topic-tree"})
 print(myname)
-#<div id="Popover-43436-11071-toggle" aria-haspopup="true" aria-expanded="false" aria-owns="Popover-43436-11071-content">机器学习</div>
-#<div aria-expanded="false" aria-haspopup="true" aria-owns="null-content" data-reactid="150" id="null-toggle">数据分析</div>
-#<span class="Field-text" data-reactid="133">临床医疗</span>
+
+
