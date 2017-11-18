a6b9002c8bb6597f95d28477dd4792bf6ff4f3c4
Juvenka <Juvenka@users.noreply.github.com>
2017-11-18 15:44:16 +0800

    11.18

--- zhihu_spider/zhihu_topic.py
+++ zhihu_spider/zhihu_topic.py
@@ -2,41 +2,49 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import json
+import http.cookiejar
 
-s = requests.Session()
 headers = {
     'Host': 'www.zhihu.com',
     'Referer': 'https://www.zhihu.com/',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36',
 }
-response = s.get("https://www.zhihu.com", headers=headers)
-soup = BeautifulSoup(response.content, "html.parser")
-xsrf = soup.find('input', attrs={"name": "_xsrf"}).get("value")
+s = requests.Session()
+s.cookies = http.cookiejar.LWPCookieJar('zhihu_cookie')
+#加载cookies
+try:
+    s.cookies.load(ignore_discard=True,ignore_expires=True)
+    r = s.get("https://www.zhihu.com/people/edit", headers=headers)
+    print(r.status_code)
+    with open("xx.html", "wb") as f:
+        f.write(r.content)
+except:
+    print('未加载')
+    #获取xsrf
+    response = s.get("https://www.zhihu.com", headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+    xsrf = soup.find('input', attrs={"name": "_xsrf"}).get("value")
+    #获取验证码并且输入
+    t = str(int(time.time() * 1000))#验证码是按时间戳命名
+    captcha_url = 'https://www.zhihu.com/captcha.gif?r='+ t + '&type=login&lang=cn'
+    print(captcha_url)
+    gifhtml = s.get(captcha_url,headers=headers)
+    with open('captcha.jpg','wb') as f:
+        f.write(gifhtml.content)
+    captcha={"img_size":[200,44],"input_points":[]}
+    input_points = [[22.796875, 22], [42.796875, 22], [63.796875, 21], [84.796875, 20], [107.796875, 20],[129.796875, 22], [150.796875, 22]]
+    seq = input('输入位置:')
+    for i in seq:
+        captcha["input_points"].append(input_points[int(i)-1])
+    captcha = json.dumps(captcha)
+    #登陆
+    postdata = {'email': '362696076@qq.com','password': 'Zxw1213#','_xsrf': xsrf,"captcha": captcha,'captcha_type': 'cn'}
+    print(postdata)
+    result = s.post('https://www.zhihu.com/login/email', data=postdata, headers=headers).json()
+    print(result['msg'])
+    #保存cookies
+    s.cookies.save(ignore_discard=True,ignore_expires=True)
 
-#获取验证码
-t = str(int(time.time() * 1000))#验证码是按时间戳命名
-captcha_url = 'https://www.zhihu.com/captcha.gif?r='+ t + '&type=login&lang=cn'
-print(captcha_url)
-gifhtml = s.get(captcha_url,headers=headers)
-with open('captcha.jpg','wb') as f:
-    f.write(gifhtml.content)
 
-captcha={"img_size":[200,44],"input_points":[]}
 
-input_points = [[22.796875, 22], [42.796875, 22], [63.796875, 21], [84.796875, 20], [107.796875, 20],[129.796875, 22], [150.796875, 22]]
 
-seq = input('输入位置:')
-for i in seq:
-    captcha["input_points"].append(input_points[int(i)-1])
-captcha = json.dumps(captcha)
-
-postdata = {'email': '362696076@qq.com','password': 'Zxw1213#','_xsrf': xsrf,"captcha": captcha,'captcha_type': 'cn'}
-print(postdata)
-result = s.post('https://www.zhihu.com/login/email', data=postdata, headers=headers).json()
-print(result['msg'])
-
-r = s.get("https://www.zhihu.com/people/edit", headers=headers)
-print(r.status_code)
-print(r.text)
-with open("xx.html", "wb") as f:
-    f.write(r.content)
--- zhihu_spider/zhihubieren.py
+++ zhihu_spider/zhihubieren.py
@@ -1,59 +1,20 @@
-# encoding: utf-8
-# !/usr/bin/env python
-
-import time
-
-import json
 import requests
 from bs4 import BeautifulSoup
-s = requests.session()
+import http.cookiejar
+
 headers = {
-    "Host": "www.zhihu.com",
-    "Referer": "https://www.zhihu.com/",
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+    'Host': 'www.zhihu.com',
+    'Referer': 'https://www.zhihu.com/',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36',
 }
-
-
-response = s.get("https://www.zhihu.com", headers=headers, verify=False)
-soup = BeautifulSoup(response.content, "html.parser")
-xsrf = soup.find('input', attrs={"name": "_xsrf"}).get("value")
-
-
-    #把验证码图片保存到当前目录，手动识别验证码
-
-t = str(int(time.time() * 1000))#验证码是按时间戳命名
-captcha_url = 'https://www.zhihu.com/captcha.gif?r=' + t + "&type=login&lang=cn"
-print(captcha_url)
-r = s.get(captcha_url, headers=headers)
-with open('captcha.gif', 'wb') as f:
-    f.write(r.content)
-    f.close()
-
-captcha = {
-    'img_size': [200, 44],
-    'input_points': [],
-}
-points = [[22.796875, 22], [42.796875, 22], [63.796875, 21], [84.796875, 20], [107.796875, 20],[129.796875, 22], [150.796875, 22]]
-seq = input('请输入倒立字的位置\n>')
-for i in seq:
-    captcha['input_points'].append(points[int(i) - 1])
-captcha = json.dumps(captcha)
-
-
-login_url = 'https://www.zhihu.com/login/email'
-data = {
-    'email': '362696076@qq.com',
-    'password': 'Zxw1213#',
-    '_xsrf': xsrf,
-    "captcha": captcha,
-    'captcha_type': 'cn'}
-
-response = s.post(login_url, data=data, headers=headers)
-login_code = response.json()
-print(login_code['msg'])
-
-r = s.get("https://www.zhihu.com/settings/profile", headers=headers)
-print(r.status_code)
-print(r.text)
-with open("xx.html", "wb") as f:
-    f.write(r.content)
+s = requests.Session()
+s.cookies = http.cookiejar.LWPCookieJar('zhihu_cookie')
+#加载cookies
+try:
+    s.cookies.load(ignore_discard=True,ignore_expires=True)
+    r = s.get("https://www.zhihu.com/people/edit", headers=headers)
+    print(r.status_code)
+    with open("xx.html", "wb") as f:
+        f.write(r.content)
+except:
+    print('未加载')
