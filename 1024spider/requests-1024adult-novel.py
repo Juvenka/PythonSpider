eee99940569d397daedf2a02992adf5ae4ad1baf
Juvenka <32841653+Juvenka@users.noreply.github.com>
2017-11-26 23:02:28 +0800

    fdc

--- 1024spider/requests-1024adult-novel.py
+++ 1024spider/requests-1024adult-novel.py
@@ -2,11 +2,21 @@ import requests
 import os
 import bs4
 firsturl = "http://1024.2048xd.info/pw/thread.php?fid=17"
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/61.0.3163.79 Safari/537.36'}
-
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.8',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Cookie': '__cfduid=dca3abd512615187557755e6e4c9b91e61509545168; aafaf_ol_offset=97; aafaf_readlog=%2C734300%2C; visid_incap_1237688=Pbqrd0t6RpK+1Ohe33h27jo+GVoAAAAAQUIPAAAAAADmt0J0AR4HdbpKp/rlaoR5; incap_ses_636_1237688=bh4CdSpO9UuhzcU3CofTCDo+GVoAAAAArEuaH5QVGCELxwPVoGOb5g==; aafaf_lastpos=F17; aafaf_lastvisit=341%091511603847%09%2Fpw%2Fthread.php%3Ffid%3D17; aafaf_threadlog=%2C3%2C17%2C; a0888_pages=2; a0888_times=3; __tins__17810888=%7B%22sid%22%3A1511603755874%2C%22vd%22%3A2%2C%22expires%22%3A1511605561640%7D; __51cke__=; __51laig__=2',
+    'Upgrade-Insecure-Requests': '1',
+    'Host': '1024.skswk9.pw',
+    'Referer': 'http://1024.skswk9.pw/pw/thread.php?fid=3',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 '
+                  '(KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36'}
 URL = requests.get(firsturl, headers=headers, timeout=None)
 html_web = bs4.BeautifulSoup(URL.content, 'html5lib')
+print(html_web)
 target = html_web.find_all(name='a', attrs={'class': 'a2 fn'})
 i = 1
 Referer = 'http://1024.2048xd.info/pw/'
@@ -17,8 +27,7 @@ for t in target:
     num[str(i)] = t.string
     i += 1
 print('请输入序号：', end=' ')
-series = num[input()]
-halfTybe = series.parent['href']
+halfTybe = num[input()].parent['href']
 Tybe = Referer + halfTybe
 Tybe_open = requests.get(Tybe, headers=headers, timeout=3)
 Tybe_page = bs4.BeautifulSoup(Tybe_open.content, 'html5lib', from_encoding='UTF-8')
@@ -41,7 +50,7 @@ for tar in tar_tag:
         except requests.Timeout as e:
             print(e)
             continue
-        f = open('C:\\Users\\Administrator\\Desktop\\erotic_novels' + os.sep + series + tar.string + '.txt', 'w')
+        f = open('C:\\Users\\Administrator\\Desktop\\erotic_novels' + os.sep + tar.string + '.txt', 'w')
         for nov in novel.strings:
             nov = nov.replace('\xa0', ' ')
             nov = nov.replace('\ue0ff', ' ')
