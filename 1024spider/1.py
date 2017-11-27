18f42dd08c474e15305b3cc116ad1917060a5322
Juvenka <32841653+Juvenka@users.noreply.github.com>
2017-11-27 22:57:23 +0800

    -

--- 1024spider/1.py
+++ 1024spider/1.py
@@ -1,17 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 #chrome_options = webdriver.ChromeOptions()
 #chrome_options.add_argument('--headless')
 #driver = webdriver.Chrome('e:\chromedriver.exe', chrome_options=chrome_options)
-driver = webdriver.Chrome('d:\chromedriver.exe')
+driver = webdriver.Chrome('e:\chromedriver.exe')
 driver.get("http://www3.uptorrentfilespacedownhostabc.net/updowm/file.php/OZYDZ9m.html")
 try:
     print(driver.current_url)
     element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "down_btn"))
+        expected_conditions.presence_of_element_located((By.ID, "down_btn"))
     )
     element.click()
     print(driver.window_handles)
