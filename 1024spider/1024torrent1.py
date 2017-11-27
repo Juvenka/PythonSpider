ef8fdc9d2a7f5437c2dcd59250c5a6a0394002bb
Juvenka <Juvenka@users.noreply.github.com>
2017-11-27 08:12:55 +0800

    1

--- 1024spider/1024torrent1.py
+++ 1024spider/1024torrent1.py
@@ -1,3 +1,5 @@
+#http://9453hot.com/forum-41-1.html
+
 import requests
 import bs4
 from selenium import webdriver
@@ -6,26 +8,26 @@ from selenium import webdriver
 #chrome_options.add_argument('--headless')
 #driver = webdriver.Chrome('D:\chromedriver.exe',chrome_options=chrome_options)
 driver = webdriver.Chrome('D:\chromedriver.exe')
-url = "http://1024.skswk9.pw/pw/thread.php?fid=3"
+url = "http://9453hot.com/forum-41-1.html"
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36'}
 r = requests.get(url,headers=headers).content
 soup = bs4.BeautifulSoup(r,'html5lib')
-tag = soup.find_all('a',attrs={'href':True,'id':True})
+tag = soup.find_all('a',attrs={'href':True,'onclick':"atarget(this)"})
+print(tag)
 for tag in tag:
         if ('騎兵' in tag.string) or ('动漫' in tag.string) or ('有碼' in tag.string) or ('灣搭' in tag.string):
-            url = 'http://1024.skswk9.pw/pw/' + tag['href']
+            url = 'http://9453hot.com/' + tag['href']
             r = requests.get(url,headers=headers).content
             soup = bs4.BeautifulSoup(r,'html5lib')
             tag = soup.find('div',attrs={'class':"tpc_content",'id':"read_tpc"}).find_all('a',attrs={'href':True,'target':"_blank"})
             for tag in tag:
                 if tag.string is not None:
-                    print(tag)
                     print(tag.string)
-                    break
                     driver.get(tag['href'])
-                    r = driver.find_element_by_xpath('//*[@id="down_btn"]').click()
+                    r = driver.find_element_by_xpath('//*[@id="btnDownload"]').click()
                     with open('1.torrent','wb') as f:
                         f.write(r.content)
                     print(tag)
                     break
             break
+
