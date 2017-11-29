bf0c0394a89eae7ed6107bf018187b2dec4fe6c2
Juvenka <Juvenka@users.noreply.github.com>
2017-11-29 16:01:07 +0800

    1

--- 1024spider/1.py
+++ 1024spider/1.py
@@ -8,7 +8,7 @@ from time import sleep
 #chrome_options.add_argument('--headless')
 #driver = webdriver.Chrome('d:\chromedriver.exe', chrome_options=chrome_options)
 driver = webdriver.Chrome('d:\chromedriver.exe')
-driver.set_page_load_timeout(2)
+driver.set_page_load_timeout(3)
 #driver.implicitly_wait(10)
 #driver.execute_async_script('document.title')
 #set_script_timeout(time_to_wait) - 在一个 execute_async_script 调用期间，设置脚本等待的时间
@@ -16,15 +16,7 @@ try:
     driver.get("http://www3.uptorrentfilespacedownhostabc.net/updowm/file.php/OZYDZ9m.html")
 except TimeoutException as e:
     print(e)
-driver.get_screenshot_as_file('C:\\Users\Administrator\Downloads\pic.png')
-
-try:
-    print(driver.current_url)
-    element = WebDriverWait(driver, 2).until(
-        expected_conditions.presence_of_element_located((By.ID, "down_btn"))
-    )
-    element.click()
-    print(driver.window_handles)
-finally:
+    driver.find_element_by_id('down_btn').click()
     driver.close()
     print(driver.window_handles)
+
--- 1024spider/2.py
+++ 1024spider/2.py
@@ -9,12 +9,14 @@ a = webdriver.Chrome('d:\chromedriver.exe')
 #driver.implicitly_wait(10)
 #driver.execute_async_script('document.title')
 #set_script_timeout(time_to_wait) - 在一个 execute_async_script 调用期间，设置脚本等待的时间
-try:
-    a.get("http://www3.uptorrentfilespacedownhostabc.net/updowm/file.php/OZYDZ9m.html")
-    wait = WebDriverWait(a, 2)
-    element = wait.until(expected_conditions.presence_of_element_located((By.ID, "down_btn")))
-    element.click()
-    print(a.window_handles)
-finally:
-    a.close()
-    print(a.window_handles)
+
+a.get("http://www3.uptorrentfilespacedownhostabc.net/updowm/file.php/OZYDZ9m.html")
+element = WebDriverWait(a, 2).until(expected_conditions.element_to_be_clickable((By.ID, "down_btn")))
+element.click()
+print(a.window_handles)
+
+a.close()
+print(a.window_handles)
+#http://www3.uptorrentfilespacedownhostabc.net/updowm/down.php?type=torrent&id=OZYDZ9m&name=ABP660
+#type=torrent&id=OZYDZ9m&name=ABP660
+#http://www3.uptorrentfilespacedownhostabc.net/?type=torrent&id=OZYDZ9m&name=ABP660
