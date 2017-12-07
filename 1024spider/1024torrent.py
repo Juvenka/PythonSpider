fc032583a56fc179c62a9811960d5e8877e67f68
Juvenka <32841653+Juvenka@users.noreply.github.com>
2017-12-07 22:12:18 +0800

    1

--- 1024spider/1024torrent.py
+++ 1024spider/1024torrent.py
@@ -8,7 +8,7 @@ url = "http://1024.skswk9.pw/pw/simple/index.php?f3.html"
 headers1 = {
     'Host': 'www3.uptorrentfilespacedownhostabc.net',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 '
-                  'Safari/537.36',}
+                  'Safari/537.36'}
 headers2 = {
     'Host': 'www3.uptorrentfilespacedownhostabc.net',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 '
@@ -32,12 +32,12 @@ for i in tag:
         url = 'http://1024.skswk9.pw/pw/' + i['href']
         r = requests.get(url, headers=headers).content
         soup = bs4.BeautifulSoup(r, 'lxml')
-        tag_ = soup.find('td', attrs={'colspan': '2','class': 'tpc_content'}).\
+        tag_ = soup.find('td', attrs={'colspan': '2', 'class': 'tpc_content'}).\
             find_all('a', attrs={'href': True, 'target': "_blank"})
         headers1['Referer'] = url
         for i_ in tag_:
             if (i_.string is not None) and (i_.string != '點擊進入下載'):
-                r = requests.get(i_['href'],headers=headers1).content
+                r = requests.get(i_['href'], headers=headers1).content
                 soup = bs4.BeautifulSoup(r, 'lxml')
                 tag_id = soup.find('input', attrs={'id': 'id'})
                 tag_name = soup.find('input', attrs={'id': 'name'})
--- 1024spider/1024torrent1.py
+++ 1024spider/1024torrent1.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-driver = webdriver.Chrome('D:\chromedriver')
+driver = webdriver.Chrome('e:\chromedriver')
 try:
     driver.get("http://www3.uptorrentfilespacedownhostabc.net/updowm/file.php/P00SFRg.html")
-    WebDriverWait(driver,1,0.1).until(EC.element_to_be_clickable((By.ID,'down_btn'))).submit()
+    WebDriverWait(driver, 1, 0.1).until(EC.element_to_be_clickable((By.ID, 'down_btn'))).click()
 finally:
     driver.close()
